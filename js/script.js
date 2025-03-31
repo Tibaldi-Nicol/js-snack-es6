baf634bf36569529e1b48bb47423d8e4ae153fad
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-31 09:51:22 +0200

    [UPD]ADD-peso

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const bikes = [
     {name : "bmc", peso: 22},
     {name : "trek", peso: 24},
 ];
+
+//definisco bici piu leggera prima di fare il ciclo di confronto
+
+let biciLeggera = bikes[0];
+console.log(biciLeggera);
+//ciclo per confrontare le bici per trovare uella con il peso minore
